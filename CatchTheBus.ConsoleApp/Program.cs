0765da55dc876371a4208aca3c3a4894c15f3725
Mikhail Dudin <dudin-m@live.ru>
2016-10-07 22:48:03 +0300

    Added an example of message sending

--- CatchTheBus.ConsoleApp/Program.cs
+++ CatchTheBus.ConsoleApp/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Configuration;
+using System.Net;
+using CatchTheBus.Service.RocketChatModels;
+using CatchTheBus.Service.Services;
 using Nancy.Hosting.Self;
+using Newtonsoft.Json;
 
 namespace CatchTheBus.ConsoleApp
 {
@@ -9,12 +14,29 @@ namespace CatchTheBus.ConsoleApp
         {
 			// new TrackScheduleTask().Execute();
 
-			/*using (var host = new NancyHost(new Uri("http://localhost:8080")))
+			using (var client = new WebClient())
 			{
+				var url = ConfigurationManager.AppSettings["IncomingWebhookUrl"];
+
+				var asdf = new MessageFromBot
+				{
+					To = "@busbot",
+					Text = "YO!!",
+					IconUrl = ConfigurationManager.AppSettings["Icon"]
+				};
+
+				client.Headers["Content-Type"] = "application/json";
+				client.UploadStringAsync(new Uri(url), JsonConvert.SerializeObject(asdf));
+			}
+
+			using (var host = new NancyHost(new Uri("http://localhost:8080")))
+			{
+				UrlBuilder.GetBaseUrl(); // костылик, чтобы ассембли с сервисом загрузилась в аппдомен
+
 				host.Start();
 				Console.WriteLine("Running on http://localhost:8080");
 				Console.ReadLine();
-			}*/
+			}
 		}
     }
 }
--- CatchTheBus.Service/RocketChatModels/MessageFromBot.cs
+++ CatchTheBus.Service/RocketChatModels/MessageFromBot.cs
@@ -1,6 +1,16 @@
-﻿namespace CatchTheBus.Service.RocketChatModels
+﻿using Newtonsoft.Json;
+
+namespace CatchTheBus.Service.RocketChatModels
 {
 	public class MessageFromBot
 	{
+		[JsonProperty("icon_url")]
+		public string IconUrl { get; set; }
+
+		[JsonProperty("text")]
+		public string Text { get; set; }
+
+		[JsonProperty("channel")]
+		public string To { get; set; }
 	}
 }
